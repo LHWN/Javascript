1e86a80f504d9ed22a252ef4812f554a00546f7b
LHWN <cjsrnr1674@gmail.com>
2018-07-25 17:49:57 +0900

    first commit

---
+++ Lecture_Javascript/node.js
@@ -0,0 +1,9 @@
+var http = require('http');
+http.createServer(function(req, res) {
+  res.writeHead(200, {
+    'Content-Type': 'text/plain'
+  });
+  res.write('Hello World\n');
+  res.end();
+}).listen(1337, 'localhost');
+console.log('Server running at http://127.0.0.1:1337/');
